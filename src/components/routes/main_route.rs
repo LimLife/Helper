a9ca439f092d37e506ecf616206515060ffd7f4c
LimLife <limfobos@gmail.com>
2025-12-30 01:50:22 +0300

    fix_main_layout

--- src/components/routes/main_route.rs
+++ src/components/routes/main_route.rs
@@ -2,16 +2,16 @@ use dioxus::prelude::*;
 use dioxus_router::{Routable};
 
 use crate::components::{elements::{
-   pages::home::Home,
-   pages::blog::Blog
-}};
+    pages::blog::Blog
+},
+    elements::layouts::main_layout::MainLayout};
 
 
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
 pub enum Route {
     #[route("/")]
-    Home {},
+    MainLayout {},
     #[route("/blog/:id")]
     Blog { id: i32 },
 }
