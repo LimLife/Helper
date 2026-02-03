288e9507b11d680a7ca81eee51777fc58a3207bf
LimLife <limfobos@gmail.com>
2026-02-04 01:10:06 +0300

    adapt_NavMenu

--- src/components/elements/layouts/main_layout/sidebar_left/language_tree/language_section/language_content/language_nav_links/nav_link.rs
+++ src/components/elements/layouts/main_layout/sidebar_left/language_tree/language_section/language_content/language_nav_links/nav_link.rs
@@ -2,8 +2,8 @@ use dioxus::prelude::*;
 
 
 #[component]
-pub fn NavLink() -> Element {
+pub fn NavLinkItem(href:String, link_name: String) -> Element {
     rsx!{
-        a { class: "lenguage-section-content-language-nav-link", href: "#", "Nav Link" }
+        a { class: "lenguage-section-content-language-nav-link", href, {link_name} }
     }
 }
--- src/components/elements/layouts/main_layout/sidebar_left/language_tree/language_section/language_content/language_nav_links/nested_nav_group.rs
+++ src/components/elements/layouts/main_layout/sidebar_left/language_tree/language_section/language_content/language_nav_links/nested_nav_group.rs
@@ -1,17 +1,76 @@
 use dioxus::prelude::*;
 
-use super::nav_link::NavLink;
+use super::nav_link::NavLinkItem;
+use crate::components::elements::ui::collapse::Collapse;
+use crate::components::models::language_content::{
+    nav_group_model::NavGroup,
+    nav_link_model::NavLink
+};
+
 
 #[component]
-pub fn NestedNavGroup() -> Element{
+pub fn NestedNavGroup() -> Element {
+   let root_group = NavGroup {
+        name: "Main Navigation".to_string(),
+        links: vec![],
+        childrens: Some(vec![
+            NavGroup {
+                name: "Languages".to_string(),
+                links: vec![
+                    NavLink { path: "Rust".to_string(), href: "#".to_string() },
+                    NavLink { path: "TypeScript".to_string(), href: "#".to_string() },
+                ],
+                childrens: Some(vec![
+                    NavGroup {
+                        name: "Frontend".to_string(),
+                        links: vec![
+                            NavLink { path: "React".to_string(), href: "#".to_string() },
+                        ],
+                        childrens: None,
+                    }
+                ]),
+            },
+            NavGroup {
+                name: "Tools".to_string(),
+                links: vec![],
+                childrens: None,
+            },
+        ]),
+    };
     rsx!{
         div { class: "lenguage-section-content-language-nav-group",
-            span { "Group Test nav link" }
-            ul {
-                li { NavLink {} }
-                li { NavLink {} }
-                li { NavLink {} }
-            }
+            NavGroupItem { group: root_group }
+        }
+    }
+}
+
+#[component]
+pub fn NavGroupItem(group: NavGroup, #[props(default = 0)]level:u32) -> Element {
+    let id_name =format!("{}-{}", group.name.clone(), level);
+    rsx!{
+        Collapse {
+            //style: format!("padding-left:{level}rem;"),
+            //key: "{group.name.clone()}-{level}",
+            id: id_name,
+            label: rsx! {
+                span { {group.name} }
+            },
+            content: rsx! {
+                if !group.links.is_empty() {
+                    ul {
+                        for link in &group.links {
+                            li {
+                                NavLinkItem { href: link.href.clone(), link_name: link.path.clone() }
+                            }
+                        }
+                    }
+                }
+                if let Some(children) = &group.childrens {
+                    for child in children {
+                        NavGroupItem { group: child.clone(), level: level + 1 }
+                    }
+                }
+            },
         }
     }
 }
